309d97a3589d750cd9c2e27f03e5bf44ccf43295
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-05-29 17:45:19 -0600

    main weather section

--- script.js
+++ script.js
@@ -9,19 +9,58 @@ var temp = document.querySelector('.temp');
 var wind = document.querySelector('.wind');
 var humidity = document.querySelector('.humidity');
 var uvIndex = document.querySelector('.uvIndex');
+var mainIcon = document.querySelector('.mainIcon')
 
 // vars for cards 
 var cardDate = document.querySelector('.cardDate')
 var weatherImg = document.querySelector('.weatherImage')
 
-var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Edison&appid=' + mykey;
+//city names
+ let atlanta = document.querySelector('.atlanta')
+ let austin = document.querySelector('.austin')
+ let chicago = document.querySelector('.chicago')
+ let newYork = document.querySelector('.newYork')
+ let orlando = document.querySelector('.orlando')
+ let sanDiego = document.querySelector('.sanDiego')
+ let seattle = document.querySelector('seattle')
 
-fetch(requestUrl)
-    .then(function(response) {
-    return response.json()
+
+var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Denver&units=imperial&appid=' + mykey;
+
+async function startAPI() {
+    var response = await fetch(requestUrl)
+    var data = await response.json()
+    .then(function(data) {
+        userCity.textContent=(data.name)
+        temp.innerHTML= Math.floor(data.main.temp)
+        wind.innerHTML= (data.wind.speed)
+        humidity.innerHTML = (data.main.humidity)
+        // uvIndex.innerHTML = (data.)
+       })
+       
+}
+
+startAPI();
+
+
+
+atlanta.addEventListener('click', async function() {
+
+    const atl = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Atlanta&units=imperial&appid=' + mykey);
+    const atlWeather = await atl.json()
+    console.log(atlWeather)
+
+});
+
+
+
+
+// fetch(requestUrl)
+//     .then(function(response) {
+//     return response.json()
 
     
-    }).then(function(data) {
-        console.log(data)
-    })
+//     }).then(function(data) {
+//         console.log(data)
+//     })
 

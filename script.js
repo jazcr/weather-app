2197a53bc80a5ec18edf78fbbf2dd253ad542b67
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-05-29 20:48:41 -0600

    converting to jquery to condense code

--- script.js
+++ script.js
@@ -1,66 +1,18 @@
 var mykey = config.MY_KEY;
 
+var initWeather = 'http://api.openweathermap.org/data/2.5/weather?q=Denver&units=imperial&appid=' + mykey;
 
-//vars for main weather section
-var searchBtn = document.querySelector('.searchBtn');
-var inputValue = document.querySelector('.inputValue')
-var userCity = document.querySelector('.userCity');
-var temp = document.querySelector('.temp');
-var wind = document.querySelector('.wind');
-var humidity = document.querySelector('.humidity');
-var uvIndex = document.querySelector('.uvIndex');
-var mainIcon = document.querySelector('.mainIcon')
-
-// vars for cards 
-var cardDate = document.querySelector('.cardDate')
-var weatherImg = document.querySelector('.weatherImage')
-
-//city names
- let atlanta = document.querySelector('.atlanta')
- let austin = document.querySelector('.austin')
- let chicago = document.querySelector('.chicago')
- let newYork = document.querySelector('.newYork')
- let orlando = document.querySelector('.orlando')
- let sanDiego = document.querySelector('.sanDiego')
- let seattle = document.querySelector('seattle')
-
-
-var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Denver&units=imperial&appid=' + mykey;
-
-async function startAPI() {
-    var response = await fetch(requestUrl)
-    var data = await response.json()
-    .then(function(data) {
-        userCity.textContent=(data.name)
-        temp.innerHTML= Math.floor(data.main.temp)
-        wind.innerHTML= (data.wind.speed)
-        humidity.innerHTML = (data.main.humidity)
-        // uvIndex.innerHTML = (data.)
-       })
-       
-}
-
-startAPI();
-
-
-
-atlanta.addEventListener('click', async function() {
-
-    const atl = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Atlanta&units=imperial&appid=' + mykey);
-    const atlWeather = await atl.json()
-    console.log(atlWeather)
-
-});
-
-
-
-
-// fetch(requestUrl)
-//     .then(function(response) {
-//     return response.json()
-
+$.ajax({
+    url: initWeather,
+    method: "GET"
+  }).then(function(response) {
     
-//     }).then(function(data) {
-//         console.log(data)
-//     })
+    $(".userCity").text(response.name);
+    $(".mainIcon").html("<img src=\"http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png\">");
+    console.log(response.weather[0].icon);
+    $(".temp").text(Math.floor(response.main.temp));
+    $(".wind").text(response.wind.speed);
+    $(".humidity").text(response.main.humidity);
+console.log(response)
 
+  })

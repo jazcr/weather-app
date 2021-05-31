2c1bcd099bcce69a8067654859788c3a970d75df
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-05-30 23:47:00 -0600

    uv index partial

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ $(".searchBtn").click(function(event) {
     event.preventDefault()
     let city = $(".inputValue").val();
     let initWeather = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + mykey;
-    let forecastAPI = 'https://api.openweathermap.org/data/2.5/onecall?lat='+ lat +'&lon='+ lon +'&exclude=hourly,minutely,alerts' + mykey;
     let lon;
     let lat;
 
@@ -33,14 +32,61 @@ $(".searchBtn").click(function(event) {
             let lat = response.coord.lat;
             let lon = response.coord.lon;
 
+            let forecastAPI = 'https://api.openweathermap.org/data/2.5/onecall?lat='+ lat +'&lon='+ lon +'&exclude=hourly,minutely,alerts&units=imperial&appid=' + mykey;
+
             $.ajax({
                 url: forecastAPI,
                 method: "GET"
             }).then(function(response) {
                 console.log(response)
-                // $(".fiveDayFore").text("");
-                // for (let i = 0; i < 5; i++) {
-                    // $(".cardTemp").text(response.)
+
+                //sections for each card -- 5 day forecast
+                $(".uvIndex").text(response.current.uvi)
+
+                let uv = (Math.floor(response.current.uvi))
+                console.log(uv);
+
+                if (uv < 3) {
+                    $(".uvIndex").addClass('uvGreen');
+                } else if (uv > 3 || uv < 6) {
+                    $(".uvIndex").addClass('uvYellow');
+                } else if (uv > 6 || uv < 8) {
+                    $(".uvIndex").addClass('uvOrange');
+                } else if (uv > 8 || uv < 11); {
+                    $(".uvIndex").addClass('uvRed');  
+                } 
+
+                    $(".cardDate0").text(moment().add(1, 'days').format('L'))
+                    $(".weatherIcon0").html("<img src=\"http://openweathermap.org/img/wn/" + response.daily[0].weather[0].icon + ".png\">");
+                    $(".cardTemp0").text(Math.floor(response.daily[0].temp.day))
+                    $(".cardWind0").text(response.daily[0].wind_speed)
+                    $(".cardHumidity0").text(response.daily[0].humidity)
+
+                    $(".cardDate1").text(moment().add(2, 'days').format('L'))
+                    $(".weatherIcon1").html("<img src=\"http://openweathermap.org/img/wn/" + response.daily[1].weather[0].icon + ".png\">");
+                    $(".cardTemp1").text(Math.floor(response.daily[1].temp.day))
+                    $(".cardWind1").text(response.daily[1].wind_speed)
+                    $(".cardHumidity1").text(response.daily[1].humidity)
+
+                    $(".cardDate2").text(moment().add(3, 'days').format('L'))
+                    $(".weatherIcon2").html("<img src=\"http://openweathermap.org/img/wn/" + response.daily[2].weather[0].icon + ".png\">");
+                    $(".cardTemp2").text(Math.floor(response.daily[2].temp.day))
+                    $(".cardWind2").text(response.daily[2].wind_speed)
+                    $(".cardHumidity2").text(response.daily[2].humidity)
+
+                    $(".cardDate3").text(moment().add(4, 'days').format('L'))
+                    $(".weatherIcon3").html("<img src=\"http://openweathermap.org/img/wn/" + response.daily[3].weather[0].icon + ".png\">");
+                    $(".cardTemp3").text(Math.floor(response.daily[3].temp.day))
+                    $(".cardWind3").text(response.daily[3].wind_speed)
+                    $(".cardHumidity3").text(response.daily[3].humidity)
+
+                    $(".cardDate4").text(moment().add(5, 'days').format('L'))
+                    $(".weatherIcon4").html("<img src=\"http://openweathermap.org/img/wn/" + response.daily[4].weather[0].icon + ".png\">");
+                    $(".cardTemp4").text(Math.floor(response.daily[4].temp.day))
+                    $(".cardWind4").text(response.daily[4].wind_speed)
+                    $(".cardHumidity4").text(response.daily[4].humidity)
+
+
             
             })
         })
@@ -48,6 +94,8 @@ $(".searchBtn").click(function(event) {
 
 
 });
+
+
 
     
 

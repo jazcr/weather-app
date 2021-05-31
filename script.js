e96b546f3a02ed780858df009b6a945c66e5d9ec
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-05-30 19:34:34 -0600

    initial weather function

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-var mykey = config.MY_KEY;
-let city = "Denver";
+let mykey = config.MY_KEY;
+// let city = "Denver";
 
-var initWeather = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + 'units=imperial&appid=' + mykey;
 
-$.ajax({
-    url: initWeather,
-    method: "GET"
-  }).then(function(response) {
-    
-    $(".userCity").text(response.name);
-    $(".mainIcon").html("<img src=\"http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png\">");
-    console.log(response.weather[0].icon);
-    $(".temp").text(Math.floor(response.main.temp));
-    $(".wind").text(response.wind.speed);
-    $(".humidity").text(response.main.humidity);
-console.log(response)
+function updateDay() {
+    var currentDate = moment().format('L');
+    $(".currentDay").html(currentDate);
+    console.log(currentDate)
+}
 
-  })
+
+
+
+function userInputWeather() {
+    // let city = $(".inputValue").val();
+    let initWeather = 'http://api.openweathermap.org/data/2.5/weather?q=Denver&units=imperial&appid=' + mykey;
+    console.log(initWeather)
+
+    $.ajax({
+        url: initWeather,
+        method: "GET"
+        }).then(function(response) {
+            lat = response.coord.lat;
+            lon = response.coord.lon;
+            $(".userCity").text(response.name);
+            $(".mainIcon").html("<img src=\"http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png\">");
+            $(".temp").text(Math.floor(response.main.temp));
+            $(".wind").text(response.wind.speed);
+            $(".humidity").text(response.main.humidity);
+            console.log(response)
+        })
+ 
+};
+
+userInputWeather();
+
